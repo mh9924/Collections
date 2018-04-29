6758ccd95b786eb025673a59f815c37cf0cd3226
Matt <xmatth13@live.com>
2018-04-29 01:37:51 -0400

    Spacing fixes

--- Controllers/PagesController.php
+++ Controllers/PagesController.php
@@ -5,11 +5,11 @@ class PagesController
 	public function home()
 	{
 		require_once('Views/Pages/home.php');
-    }
+	}
 
     public function error()
 	{
 		require_once('Views/Pages/error.php');
-    }
+	}
 }
 ?>
